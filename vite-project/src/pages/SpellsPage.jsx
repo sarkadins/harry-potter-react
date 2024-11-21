0287335e85a1e57a40220d2dd2a4f06227bec3ef
Soma Sarkadi Nagy <135955407+sarkadins@users.noreply.github.com>
2024-11-21 10:36:32 +0100

    implement SpellsPage.jsx, SpellList.jsx

--- vite-project/src/pages/SpellsPage.jsx
+++ vite-project/src/pages/SpellsPage.jsx
@@ -1,6 +1,32 @@
+import { useEffect, useState } from "react"
+import SpellList from "../components/SpellList"
+
 function SpellsPage() {
+  const [spells, setSpells] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchSpellsData = async () => {
+      try {
+        const response = await fetch('https://hp-api.onrender.com/api/spells');
+        const spellsData = await response.json();
+        setSpells(spellsData);
+      } catch (error) {
+        console.log(error);
+        
+      }
+    }
+    fetchSpellsData();
+    setLoading(false)
+  }, [])
   return (
-    <div>SpellsPage</div>
+    <div>
+      {loading ? (<p>...Loading</p>) : (
+        <nav>
+          <SpellList spells={spells} />
+        </nav>
+      )}
+    </div>
   )
 }
 
